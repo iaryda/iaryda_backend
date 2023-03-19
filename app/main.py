84f69899d5afcc8a36b8d635090cf4f3816d60d1
kimsh1017 <kimsh8580@gmail.com>
2023-03-19 11:58:53 +0000

    CREATE: Create responseModel

--- app/main.py
+++ app/main.py
@@ -1,24 +1,37 @@
 from fastapi import FastAPI
-from pymongo import MongoClient
-from models import Diary
+# from pymongo import MongoClient
+from motor import motor_asyncio
+from models import Diary,diary_helper,responseModel,ErrorResponseModel
+
+app = FastAPI()
 
 host = 'localhost'
 port = 27017
 
-app = FastAPI()
-client = MongoClient(host,port)
 
-db = client['iaryda']
+# client = MongoClient(host,port)
+# db = client['iaryda']
 
-@app.get("/")
-async def main():
-    return {"message" : "hello world"}
+client = motor_asyncio.AsyncIOMotorClient(host,port)
+db = client.iaryda
+diary_collection = db.get_collection("diary")
 
-@app.get("/mongo")
-async def mongo():
-    return str(db['diary'].find_one())
+#프로젝트 파일 분리 필요, routes and database ....
 
-@app.post("/diarys")
-async def mongo(diary: Diary):
-    result = db['diary'].insert_one({"date" : diary.date, "content" : diary.content, "feeling": diary.feeling})
-    return str(result)
+@app.get("/diaries")
+async def get_all_diaries():
+    response_message = "all diaries"
+    result_data = []
+    
+    async for diary in diary_collection.find():
+        result_data.append(diary_helper(diary))
+        
+    return responseModel(response_message, result_data)
+
+@app.post("/diaries")
+async def create_diary(diary: Diary):
+    response_message = "create diary"
+    diary = await diary_collection.insert_one({"date" : diary.date, "content" : diary.content, "feeling": diary.feeling})
+    result_data = diary_helper(await diary_collection.find_one({"_id" : diary.inserted_id}))
+    return responseModel(response_message, result_data)
+
--- app/models.py
+++ app/models.py
@@ -4,3 +4,21 @@ class Diary(BaseModel):
     date: str
     content: str
     feeling: str
+        
+def diary_helper(diary) -> dict:
+    diary.pop("_id")
+    return diary
+
+def responseModel(message,data) -> dict:
+    return {
+        "status" : 200,
+        "message" : message,
+        "data" : [data]
+    }
+
+def ErrorResponseModel(error_code,message) -> dict:
+    return {
+        "status" : error_code,
+        "message" : message,
+        "data" : []
+    }
